57e82df63d4b9c811d4ed27c979890349f81bbcd
BintoroCA <bintoro1106@gmail.com>
2022-04-20 02:19:40 +0700

    fix server env

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const API = axios.create({
   // baseURL: 'http://localhost:5005/api/v1/',
-  baseURL: 'https://dmbt.herokuapp.com/api/v1/'||'http://localhost:5005/api/v1/',
+  baseURL: process.env.reactserver ||'http://localhost:5005/api/v1/',
 });
 
 export const setAuthToken = (token) => {
